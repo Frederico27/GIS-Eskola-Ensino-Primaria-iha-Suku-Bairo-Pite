7bd345d827486609c40aaff2b6990da0abf75b72
Frederico27 <fredericomrcal27@gmail.com>
2022-06-27 18:03:18 +0900

    Halo diferensa marker eskola diak ho ladiak

--- teste.php
+++ teste.php
@@ -1,57 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Quick Start - Leaflet</title>
-
-    <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
-
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
-
-    <style>
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-        }
-
-        .leaflet-container {
-            height: 400px;
-            width: 600px;
-            max-width: 100%;
-            max-height: 100%;
-        }
-    </style>
-
-
-</head>
-
-<body>
-
-
-
-    <div id="map" style="width: 600px; height: 400px;"></div>
-    <script>
-        var map = L.map('map').setView([-8.551103, 125.570769], 13);
-
-        var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1
-        }).addTo(map);
-        var marker = L.marker([-8.551103, 125.570769]).addTo(map).
-        bindPopup("Hello");
-    </script>
-
-
-
-</body>
-
-</html>
+<?php
+            include "koneksaun.php";
+            $query = $conn->query("SELECT * FROM eskola a, populasaun b, artikel c, detallu d WHERE a.id_eskola = d.id_eskola AND b.id_populasaun = d.id_populasaun AND c.id_artikel = d.id_artikel");
+            while ($dadus = $query->fetch_array()) {
+              $lng   = $dadus['longitude'];
+              $lat   = $dadus['latitude'];
+              $naran = $dadus['naran_eskola'];
+              $img   = $dadus['imajen'];
+              $tipu  = $dadus['tipu_eskola'];
+              $kond  = $dadus['kondisaun'];
+              $func1 = $dadus['funsionariu_estadu'];
+              $func2 = $dadus['funsionariu_kontratadu'];
+              $aln   = $dadus['total_alunos'];
+              $id_artc = $dadus['id_artikel'];
+            }
