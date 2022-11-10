e5f2c33a4b0ac3435eb718fa3c3c325c34c01023
Frederico27 <fredericomrcal27@gmail.com>
2022-11-10 21:27:06 +0900

    fix dynamic_title

--- dynamic_title.php
+++ dynamic_title.php
@@ -2,10 +2,10 @@
 
 if ($_GET['module'] == 'home') {
     $title = "Pajina Prinsipal";
-} else if  ($_GET['module'] == 'mapa') {
+} else if ($_GET['module'] == 'mapa') {
     $title = "Mapamentu";
-}else if  ($_GET['module'] == "artigu" OR "main-artigu") {
-    $title = "Artigu";
-}else if  ($_GET['module'] == 'login') {
+} else if ($_GET['module'] == 'login') {
     $title = "login";
+} else if ($_GET['module'] == 'artigu' || 'main-artigu') {
+    $title = "Artigu";
 }
